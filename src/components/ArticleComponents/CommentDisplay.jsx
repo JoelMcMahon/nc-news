6dc92d4bc42629cd55d5819b36018bd0e7b16ffb
JoelM <joelmcmahon2@gmail.com>
2021-12-02 14:58:34 +0000

    add formatting to single article page

--- src/components/ArticleComponents/CommentDisplay.jsx
+++ src/components/ArticleComponents/CommentDisplay.jsx
@@ -1,79 +1,72 @@
-import React, { useState, useEffect } from 'react'
-import { postComment, getComments } from '../utils/api'
+import React, { useState, useEffect } from "react";
+import { postComment, getComments } from "../utils/api";
 
 const CommentDisplay = ({ article_id, isLoggedIn, user }) => {
+  const [comments, setComments] = useState([]);
+  const [commentBody, setCommentBody] = useState("");
+  const [isError, setIsError] = useState(false);
 
-    const [comments, setComments] = useState([])
-    const [commentBody, setCommentBody] = useState("")
-    const [isError, setIsError] = useState(false)
+  useEffect(() => {
+    setIsError(false);
+    getComments(article_id)
+      .then((res) => {
+        setComments(res);
+      })
+      .catch((err) => {
+        setIsError(true);
+      });
+  }, [article_id, commentBody]);
 
-    useEffect(() => {
-        setIsError(false)
-        getComments(article_id).then((res) => {
-            setComments(res)
-        })
-        .catch((err) => {
-            setIsError(true)
-        })
-    }, [article_id, commentBody])
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    // setComments((currentComments) => {
+    //     const newCommentList = [...currentComments, ]
+    // })
+    postComment(article_id, user, commentBody).then((res) => {
+      console.log(res);
+    });
+    setCommentBody("");
+  };
 
+  //Work out how to rerender the commentDisplay after a comment has been posted -
+  //Including commentBody state in dependency array of useEffect doesn't seem to work
+  //Possibly try to optimistically render the comment? Date could be a problem..
 
-    const handleOnSubmit = (e) => {
-        e.preventDefault()
-        // setComments((currentComments) => {
-        //     const newCommentList = [...currentComments, ]
-        // })
-        postComment(article_id, user, commentBody).then((res) => {
-            console.log(res)
-        })
-        setCommentBody('');
-        
-    }
+  const handleOnChange = (e) => {
+    setCommentBody(e.target.value);
+  };
 
+  return (
+    <div>
+      <h2>Comments ({comments.length})</h2>
+      {isLoggedIn && (
+        <form onSubmit={handleOnSubmit}>
+          <label htmlFor="commentBody">Comment as {user}:</label>
+          <input
+            type="text"
+            size="20"
+            required
+            value={commentBody}
+            onChange={handleOnChange}
+          />
+          <button>Post Comment</button>
+        </form>
+      )}
 
-    //Work out how to rerender the commentDisplay after a comment has been posted -
-    //Including commentBody state in dependency array of useEffect doesn't seem to work
-    //Possibly try to optimistically render the comment? Date could be a problem..
+      <ul>
+        {comments.map((comment) => {
+          return (
+            <li>
+              <h3>{comment.author}</h3>
+              <p>{comment.created_at.slice(0, 10)}</p>
+              <p>{comment.body}</p>
+              <button>Votes {comment.votes}</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
-    
-
-
-
-    const handleOnChange = (e) => {
-        setCommentBody(e.target.value)
-    }
-
-
-
-    return (
-        <div>
-            <h2>Comments ({comments.length})</h2>
-            {isLoggedIn &&
-                <form onSubmit={handleOnSubmit}>
-                    <label htmlFor="commentBody">Comment as {user}:</label>
-                    <input
-                        type="text"
-                        size="20"
-                        required
-                        value={commentBody}
-                        onChange={handleOnChange}
-                    />
-                    <button>Post Comment</button>
-                </form>
-            }
-
-            <ul>
-                {comments.map((comment) => {
-                    return <li>
-                        <h3>{comment.author}</h3>
-                        <p>{comment.created_at}</p>
-                        <p>{comment.body}</p>
-                        <button>Votes {comment.votes}</button>
-                    </li>
-                })}
-            </ul>
-        </div>
-    )
-}
-
-export default CommentDisplay
+export default CommentDisplay;
--- src/components/ArticleComponents/SingleArticle.jsx
+++ src/components/ArticleComponents/SingleArticle.jsx
@@ -1,64 +1,104 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router'
-import { addVotes, getArticle } from '../utils/api'
-import CommentDisplay from './CommentDisplay'
-import { BiUpvote, BiDownvote } from 'react-icons/bi'
-
-
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import { addVotes, getArticle } from "../utils/api";
+import CommentDisplay from "./CommentDisplay";
+import { BiUpvote, BiDownvote } from "react-icons/bi";
+import {
+  MdOutlineModeComment,
+  MdThumbUpOffAlt,
+  MdThumbDownOffAlt,
+} from "react-icons/md";
 
 const SingleArticle = ({ isLoggedIn, user }) => {
+  const [selectedArticle, setSelectedArticle] = useState([]);
+  const [voteChange, setVoteChange] = useState(0);
+  const [isError, setIsError] = useState(false);
 
-    const [selectedArticle, setSelectedArticle] = useState([])
-    const [voteChange, setVoteChange] = useState(0)
-    const [isError, setIsError] = useState(false)
+  const { article_id } = useParams();
 
-    const { article_id } = useParams()
+  useEffect(() => {
+    setIsError(false);
+    getArticle(article_id)
+      .then((res) => {
+        setSelectedArticle(res);
+      })
+      .catch((err) => {
+        setIsError(true);
+      });
+  }, [article_id]);
 
-    useEffect(() => {
-        setIsError(false)
-        getArticle(article_id).then((res) => {
-            setSelectedArticle(res)
-        }).catch((err) => {
-            setIsError(true)
-        })
-    }, [article_id])
+  const incVote = (e) => {
+    let num = e.currentTarget.value === "upvote" ? 1 : -1;
+    setVoteChange((currVoteChange) => currVoteChange + num);
+    addVotes(article_id, num);
+  };
+  console.log(voteChange);
 
-    const incVote = (e) => {
-        let num = (e.currentTarget.value === "upvote" ? 1 : -1)
-        setVoteChange((currVoteChange) => currVoteChange + num)
-        addVotes(article_id, num)
-    }
-    console.log(voteChange)
+  return (
+    <div>
+      {isError ? (
+        <p>Sorry, that article doesn't exist</p>
+      ) : (
+        <>
+          <h2 className="main__singleArticle_title">{selectedArticle.title}</h2>
+          <div className="main__article_tags">
+            <h3 className="main__author_name">{selectedArticle.author}</h3>
+            <Link
+              className="main__topic_tag-link"
+              to={`/articles/topics/${selectedArticle.topic}`}
+            >
+              <button className="main__topic_tag">
+                {selectedArticle.topic}
+              </button>
+            </Link>
+            <p className="main__date">{selectedArticle.created_at}</p>
+            <div className="main__article_interactions--dropdown">
+              <Link to={`/articles/${selectedArticle.article_id}`}>
+                <p className="main__interaction_indicator">
+                  <MdOutlineModeComment className="main__interaction_icon" />
+                  {selectedArticle.comment_count}
+                </p>
+              </Link>
+              <Link to={`/articles/${selectedArticle.article_id}`}>
+                <p className="main__interaction_indicator">
+                  <MdThumbUpOffAlt className="main__interaction_icon" />
+                  {selectedArticle.votes + voteChange}
+                </p>
+              </Link>
+            </div>
+          </div>
+          <p className="main__singleArticle_body">{selectedArticle.body}</p>
 
-    return (
+          {isLoggedIn && (
+            <div>
+              <button
+                onClick={incVote}
+                value="upvote"
+                className="main__interaction_button"
+              >
+                <MdThumbUpOffAlt className="main__interaction_icon" />
+              </button>
+              <button
+                onClick={incVote}
+                value="downvote"
+                className="main__interaction_button"
+              >
+                <MdThumbDownOffAlt className="main__interaction_icon" />
+              </button>
+            </div>
+          )}
 
-        <div>
-            {
-                (isError ?
-                    
-                    <p>Sorry, that article doesn't exist</p>
-                    : <>
-                        <h2>{selectedArticle.title}</h2>
-                        <h3>{selectedArticle.author}</h3>
-                        <p>{selectedArticle.created_at}</p>
-                        <p>{selectedArticle.topic}</p>
-                        <p>Comments {selectedArticle.comment_count}</p>
-                        <p>{selectedArticle.body}</p>
-                        <p>Votes {selectedArticle.votes + voteChange}</p>
+          <CommentDisplay
+            id="comment_display"
+            article_id={article_id}
+            isLoggedIn={isLoggedIn}
+            user={user}
+          />
+        </>
+      )}
+    </div>
+  );
+};
 
-                        {isLoggedIn &&
-                            <>
-                                <button onClick={incVote} value="upvote"><span><BiUpvote /></span></button>
-                                <button onClick={incVote} value="downvote"><BiDownvote /></button>
-                            </>
-                        }
-
-                        <CommentDisplay id="comment_display" article_id={article_id} isLoggedIn={isLoggedIn} user={user} />
-                    </>
-                )}
-        </div>
-    )
-}
-
-export default SingleArticle
+export default SingleArticle;
