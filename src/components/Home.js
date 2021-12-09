fb6c53ba7a384f18fa8c2803891e92eccdaa34ca
JoelM <joelmcmahon2@gmail.com>
2021-12-09 13:06:16 +0000

    add custom loading component to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,71 +1,81 @@
-import React, { useEffect, useState } from 'react'
-import ArticleBody from './ArticleComponents/ArticleBody';
-import { getArticles } from './utils/api'
-import { Link } from 'react-router-dom';
-import { MdOutlineModeComment, MdThumbUpOffAlt } from 'react-icons/md'
-
+import React, { useEffect, useState } from "react";
+import ArticleBody from "./ArticleComponents/ArticleBody";
+import { getArticles } from "./utils/api";
+import { Link } from "react-router-dom";
+import { MdOutlineModeComment, MdThumbUpOffAlt } from "react-icons/md";
+import Loader from "./Loader";
 
 const Home = ({ articles, setArticles }) => {
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const [isError, setIsError] = useState(false)
+  useEffect(() => {
+    setIsError(false);
+    getArticles()
+      .then((res) => {
+        setArticles(res);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsError(true);
+      });
+  }, []);
 
+  const recentArticles = [...articles].slice(0, 3);
 
-    useEffect(() => {
-        setIsError(false)
-        getArticles().then((res) => {
-            setArticles(res)
-        })
-            .catch((err) => {
-                setIsError(true)
-            })
+  if (isLoading) return <Loader></Loader>;
 
-    }, [])
+  return (
+    <div>
+      {isError ? (
+        <p>Oops! Something went wrong... Try again!</p>
+      ) : (
+        <>
+          <h2 className="main__subheader">Latest News</h2>
+          <ul>
+            {recentArticles.map((article) => {
+              return (
+                <li key={article.article_id} className="main__article_li">
+                  <Link
+                    className="main__article_title_link"
+                    to={`/articles/${article.article_id}`}
+                  >
+                    <h2 className="main__article_title">{article.title}</h2>
+                  </Link>
+                  <div className="main__article_tags">
+                    <h3 className="main__author_name">{article.author}</h3>
+                    <Link
+                      className="main__topic_tag-link"
+                      to={`/articles/topics/${article.topic}`}
+                    >
+                      <button className="main__topic_tag">
+                        {article.topic}
+                      </button>
+                    </Link>
+                  </div>
+                  <ArticleBody article_id={article.article_id} />
+                  <div className="main__article_interactions">
+                    <Link to={`/articles/${article.article_id}`}>
+                      <p className="main__interaction_indicator">
+                        <MdOutlineModeComment className="main__interaction_icon" />
+                        {article.comment_count}
+                      </p>
+                    </Link>
+                    <Link to={`/articles/${article.article_id}`}>
+                      <p className="main__interaction_indicator">
+                        <MdThumbUpOffAlt className="main__interaction_icon" />
+                        {article.votes}
+                      </p>
+                    </Link>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+};
 
-    const recentArticles = [...articles].slice(0, 3);
-
-    return (
-        <div>
-
-            {
-                (isError ?
-
-                    <p>Oops! Something went wrong... Try again!</p>
-
-                    :
-                    <>
-                        <h2 className="main__subheader">Latest News</h2>
-                        <ul>
-                            {recentArticles.map((article) => {
-                                return (
-                                    <li key={article.article_id} className="main__article_li">
-                                        <Link className="main__article_title_link" to={`/articles/${article.article_id}`}>
-                                            <h2 className="main__article_title">{article.title}</h2>
-                                        </Link>
-                                        <div className="main__article_tags">
-                                            <h3 className="main__author_name">{article.author}</h3>
-                                            <Link className="main__topic_tag-link" to={`/articles/topics/${article.topic}`}>
-                                                <button className="main__topic_tag">{article.topic}</button>
-                                            </Link>
-                                        </div>
-                                        <ArticleBody article_id={article.article_id} />
-                                        <div className="main__article_interactions">
-                                            <Link to={`/articles/${article.article_id}`}>
-                                                <p className="main__interaction_indicator"><MdOutlineModeComment className="main__interaction_icon" />{article.comment_count}</p>
-                                            </Link>
-                                            <Link to={`/articles/${article.article_id}`}>
-                                                <p className="main__interaction_indicator"><MdThumbUpOffAlt className="main__interaction_icon" />{article.votes}</p>
-                                            </Link>
-                                        </div>
-                                    </li>
-                                )
-                            })}
-                        </ul>
-                    </>
-                )}
-        </div>
-    )
-}
-
-export default Home
-
-
+export default Home;
---
+++ src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="main__home_loading-icon">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
